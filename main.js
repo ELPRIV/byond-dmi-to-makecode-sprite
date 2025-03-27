51c1e67d6b695eb45a13a05c26df701b55db902a
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-27 08:49:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const canvas = document.getElementById("text")
 
 const fileInput = document.getElementById('myFile');
 fileInput.onchange = () => {
-  const selectedFile = fileInput.files[0];
+  const selectedFile = fileInput.files[0].getAsBinary();
   canvas.innerHTML = selectedFile;
 }
