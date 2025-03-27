0540f1da2fb3acc25193e952f9dd70282e79c0f1
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-27 10:24:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,5 +7,6 @@ const canvas = document.getElementById("text")
 const fileInput = document.getElementById('myFile');
 fileInput.onchange = () => {
   const selectedFile = fileInput.files[0];
-  canvas.innerHTML = selectedFile;
+  Metadata metadata = ImageMetadataReader.readMetadata(selectedFile);
+  canvas.innerhtml = metadata;
 }
