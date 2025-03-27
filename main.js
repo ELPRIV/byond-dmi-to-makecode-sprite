4ed80130d6c3b22631f66fa423c61a0d0c837f88
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-27 08:35:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-let mode = "full-width"
-// https://makecode.com/_EvPP98M4pYEC
-
-/**
- * To do:
- * 
- * 1. Implement "fit", "fill", "custom"
- * 2. Black and white mode
- * 3. Other cool effects
- * 4. Shuffle colors
- */
 
 const canvas = document.querySelector("canvas")
+canvas.innerHTML = "Hello JavaScript!";
