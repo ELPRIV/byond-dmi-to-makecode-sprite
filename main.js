18efbbff828b3e11e4d7038c53a2414a3cd6124a
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-27 10:36:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-import * as ExifReader from 'exifreader';
 
 const canvas = document.getElementById("text")
 
 const fileInput = document.getElementById('myFile');
 fileInput.onchange = () => {
   const selectedFile = fileInput.files[0];
-  Metadata metadata = ImageMetadataReader.readMetadata(selectedFile);
-  canvas.innerhtml = metadata;
+  canvas.innerhtml = "test";
 }
