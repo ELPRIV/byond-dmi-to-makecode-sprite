0d02ad2fa3f8abd436e504ff26068e04417da8e0
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-28 10:12:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import *
 
-const canvas = document.getElementById("text");
+let canvas = document.getElementById("text");
 
-const fileInput = document.getElementById('myFile');
+let fileInput = document.getElementById('myFile');
 fileInput.onchange = () => {
-  const selectedFile = fileInput.files[0];
+  let selectedFile = fileInput.files[0];
   canvas.innerhtml = "test";
   Metadata metadata = PNGMetadataReader.readMetadata(selectedFile);
   console.log(metadata);
