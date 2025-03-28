f2c9958ee8d7e1c0d0245b3ffd54a5151c3a544e
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-28 10:28:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ let fi = document.getElementById('myFile');
 window.document.getElementById('myFile').onchange = () => {
   let selectedFile = fi.files[0];
   canv.innerhtml = "test";
-  Metadata metadata = PNGMetadataReader.readMetadata(selectedFile);
+  let metadata = PNGMetadataReader.readMetadata(selectedFile);
   console.log(metadata);
 }
