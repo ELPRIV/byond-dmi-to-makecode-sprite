f887bf34a1bbb9b7e8484d70594d85094d0535e8
ELPRIV <93782592+ELPRIV@users.noreply.github.com>
2025-03-28 08:36:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+import PngToy from 'pngtoy';
+let a = []
+async function readZtxt(file) {
+            const pngtoy = new PngToy();
+            await pngtoy.fetch(file);
+            const zTXtChunks = pngtoy.getChunk('zTXt');
+            if (zTXtChunks) {
+                zTXtChunks.forEach(chunk => {
+                    console.log(`Keyword: ${chunk.keyword}, Text: ${chunk.text}`);
+                    a.append([chunk.keyword,chunk.text]),
+                });
+            } else {
+              console.log('No zTXt chunks found.');
+            }
+        }
 
 const canvas = document.getElementById("text")
 
